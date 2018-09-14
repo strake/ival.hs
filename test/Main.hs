7b43566a926cd723ab70ad7dbc8f1bbd3afb9e85
M Farkas-Dyck <strake888@gmail.com>
2018-09-14 14:25:44 -0600

    test

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,38 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
 
+import Prelude hiding (Num (..))
+import Algebra
+import Control.Applicative
+import Data.Interval
+import Data.Ratio
+import Data.Semigroup (Product)
+import Relation.Binary.Comparison as A
 import Test.SmallCheck
+import Test.SmallCheck.Series
 import Test.Tasty
 import Test.Tasty.SmallCheck
 
 main :: IO ()
-main = defaultMain $ testGroup "" []
+main = defaultMain $
+    testGroup ""
+    [testProperty "≤-reflexive" $ \ (a :: Interval Rational) -> a ≤ a,
+     testProperty "≤-antisymmetric" $ \ (a :: Interval Rational) b -> (a ≤ b && b ≤ a) ≡ (a ≡ b),
+     testProperty "≤-transitive" $ \ (a :: Interval Rational) b c -> not (a ≤ b && b ≤ c) || a ≤ c]
+
+instance (Serial m a, PartialOrd a) => Serial m (Interval a) where
+    series = [a :–: b | a <- series, b <- series, a ≤ b]
+
+instance (PartialOrd a, Semigroup (Product a)) => Preord (Ratio a) where
+    (liftA2 (,) numerator denominator -> (an, ad)) ≤ (liftA2 (,) numerator denominator -> (bn, bd)) =
+        an * bd ≤ bn * ad
+
+instance (A.PartialOrd a, A.Eq a, Semigroup (Product a)) => A.Eq (Ratio a)
+
+instance (PartialOrd a, Semigroup (Product a)) => PartialOrd (Ratio a) where
+    tryCompare (liftA2 (,) numerator denominator -> (an, ad)) (liftA2 (,) numerator denominator -> (bn, bd)) =
+        tryCompare (an * bd) (bn * ad)
